4a3c11fcd6d412b8b3ba4f64f06db0bea7d05c9b
Giant <giant@giantit.nl>
2012-09-22 02:03:29 +0200

    Start work on Rift loading algorithm.

---
+++ com/github/Icyene/Storm/Earthquake/Tasks/RiftLoader.java
@@ -0,0 +1,21 @@
+package com.github.Icyene.Storm.Earthquake.Tasks;
+
+import com.github.Icyene.Storm.Earthquake.Quake;
+
+/**
+ *
+ * @author Giant
+ */
+public class RiftLoader implements Runnable {
+	
+	private Quake q;
+	
+	public RiftLoader(Quake q) {
+		this.q = q;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+}
