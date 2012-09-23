e2b1b64e7547c1958a93be361ad6e207da9b2735
Giant <giant@giantit.nl>
2012-09-23 02:12:15 +0200

    Finish QuakeFinishEvent.

--- com/github/Icyene/Storm/Earthquake/Events/QuakeFinishEvent.java
+++ com/github/Icyene/Storm/Earthquake/Events/QuakeFinishEvent.java
@@ -1,9 +1,60 @@
 package com.github.Icyene.Storm.Earthquake.Events;
 
+import com.github.Icyene.Storm.Earthquake.Quake;
+import com.github.Icyene.Storm.Pair;
+import org.bukkit.World;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
 /**
  *
  * @author Giant
  */
-public class QuakeFinishEvent {
+public class QuakeFinishEvent extends Event {
+	
+	private static final HandlerList handlers = new HandlerList();
+	
+	private Quake q;
+	private World w;
+	private Pair<Integer, Integer> e;
+	private Pair<Integer, Integer> p1;
+	private Pair<Integer, Integer> p2;
+	
+	public QuakeFinishEvent(Quake q) {
+		this.q = q;
+		this.w = q.getWorld();
+		this.e = q.getEpicenter();
+		this.p1 = q.getPointOne();
+		this.p2 = q.getPointTwo();
+	}
+	
+	public Quake getQuake() {
+		return this.q;
+	}
+	
+	public World getWorld() {
+		return this.w;
+	}
+	
+	public Pair<Integer, Integer> getEpicenter() {
+		return this.e;
+	}
+	
+	public Pair<Integer, Integer> getPointOne() {
+		return this.p1;
+	}
+	
+	public Pair<Integer, Integer> getPointTwo() {
+		return this.p2;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 	
 }
