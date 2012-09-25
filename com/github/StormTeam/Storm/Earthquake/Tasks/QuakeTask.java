cec68d1d2e1dd8502fe56721364121d51768ecbb
Giant <giant@giantit.nl>
2012-09-25 23:40:18 +0200

    Continue work on getting calculations right. Might want to not subtract if distance is 40.

--- com/github/StormTeam/Storm/Earthquake/Tasks/QuakeTask.java
+++ com/github/StormTeam/Storm/Earthquake/Tasks/QuakeTask.java
@@ -17,6 +17,8 @@ public class QuakeTask implements Runnable {
 	private Storm s;
 	private Quake q;
 	private int i = 0;
+	private boolean dbg = true;
+	private int t = 10;
 	
 	public QuakeTask(Quake q, Storm s) {
 		this.q = q;
@@ -37,10 +39,40 @@ public class QuakeTask implements Runnable {
 			if(!q.isQuaking(p.getLocation()))
 				continue;
 			
-			int x = q.getEpicenter().LEFT - p.getLocation().getBlockX() + 1;
-			int z = q.getEpicenter().RIGHT - p.getLocation().getBlockZ() + 1;
+			int x = 40 - Math.abs(q.getEpicenter().LEFT - p.getLocation().getBlockX());
+			int z = 40 - Math.abs(q.getEpicenter().RIGHT - p.getLocation().getBlockZ());
 			
-			int a = 5 / ((x + z) / 2);
+			float d = ((x + z) / 2);
+			d = (d < 1) ? 1 : d;
+			float step = 5 / d;
+			step = (step < 1) ? step * 10 : step;
+			int a = 6 - (int) Math.round(step);
+			a = (a < 0) ? 0 : ((a > 5) ? 5 : a);
+			
+			
+			if(dbg) {
+				dbg = false;
+				t = 10;
+				s.getLogger().severe("========= DEBUG ========");
+				s.getLogger().severe("====== Quake =====");
+				s.getLogger().severe("Epicenter X: " + q.getEpicenter().LEFT);
+				s.getLogger().severe("Epicenter Z: " + q.getEpicenter().RIGHT);
+				s.getLogger().severe("====== Player =====");
+				s.getLogger().severe("Location X: " + p.getLocation().getBlockX());
+				s.getLogger().severe("Location Z: " + p.getLocation().getBlockZ());
+				s.getLogger().severe("====== Calculations =====");
+				s.getLogger().severe("x = q.X - p.X + 1 == " + x);
+				s.getLogger().severe("z = q.Z - p.Z + 1 == " + z);
+				s.getLogger().severe("d = ((x + z) / 2) == " + d);
+				s.getLogger().severe("a = 5 / d == " + a);
+			}else{
+				--t;
+				
+				if(t == 0) {
+					dbg = true;
+				}
+			}
+			
 			
 			if(i == 0) {
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2, a), true);
